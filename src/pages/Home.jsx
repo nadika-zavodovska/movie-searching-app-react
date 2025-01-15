140d4dcecaa44b2f59aa299fcdf64585d6d8d45d
nadika <nadikazavodovska@gmail.com>
2025-01-15 16:21:13 +0000

    Start project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 1, title: "Terminator", release_date: "1999" },
+    { id: 1, title: "Matrix", release_date: "1998" },
+  ];
+  const handleSearch = () => {
+    alert(searchQuery);
+  };
+  return (
+    <div className="home">
+      <form
+        onSubmit={handleSearch}
+        className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
+      <button
+        type="submit"
+        className="search-button">
+        Search
+      </button>{" "}
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Home;
