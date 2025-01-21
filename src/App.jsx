db5e4154d42a0d261e7fda87969fb8d3700958b6
nadika <nadikazavodovska@gmail.com>
2025-01-21 10:13:04 +0000

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './App.css';
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
-    return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />}/>
-      </Routes>
-    </main>
+  return (
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/favorites"
+            element={<Favorites />}
+          />
+        </Routes>
+      </main>
+    </div>
   );
-};
+}
 export default App;
